e4aae34a8a8b716623264155387eff20b9338a61
maxuellchaves <maxuellchaves28@gmail.com>
2024-11-08 14:21:02 -0300

    novos arquivos

---
+++ exercicioMudarcor/script.js
@@ -0,0 +1,16 @@
+
+function mudarCorAzul(){
+document.querySelector("body").style.backgroundColor ="blue";
+document.querySelector("h3").innerHTML = "Azul significa calma";
+}
+
+function mudarCorVerde(){
+    document.querySelector("body").style.backgroundColor ="green";
+    document.querySelector("h3").innerHTML = "Verde siginifica natureza";
+    }
+
+    function mudarCorLaranja(){
+        document.querySelector("body").style.backgroundColor ="orange";
+        document.querySelector("h3").innerHTML = "laranja siginifica força e energia";
+        }
+    
